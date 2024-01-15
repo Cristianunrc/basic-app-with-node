b8ec264705de681a5f5916edb44f21a5eab42530
Cristianunrc <cristiankhan23@gmail.com>
2024-01-15 18:06:11 -0300

    Add security in endpoint send-email

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,21 +9,19 @@ router.get('/about', (req, res) => res.render('about', { title: 'About' }))
 
 router.get('/contact', (req, res) => res.render('contact', { title: 'Contact' }))
 
-router.get('/success', (req, res) => res.render('success'))
+// router.get('/success', (req, res) => res.render('success', { title: 'Response' }))
 
 router.post('/send-email', async (req, res) => {
+  
   const { email, message } = req.body;
 
-  const contentHTML =  `
-    <h1>Email information</h1>
-    <ul>
-      <li>Email: ${email}</li>
-    </ul>
-    <p>${message}</p>
-  `;
+  if (!email || !message) {
+    const response = res.status(400).send('success', {errorMessage: 'Email and message are required'})
+    return response
+  }
 
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com', // forwardemail.net
+    host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
@@ -35,13 +33,11 @@ router.post('/send-email', async (req, res) => {
   const info = await transporter.sendMail({
     from: 'cristiankhan23@gmail.com',
     to: 'cristiankhan23@gmail.com',
-    subject: 'Website content form',
-    text: 'Hello world'
+    subject: 'New message of NodeJS app',
+    text: `The user with email ${email} says:\n\n${message}`
   })
-
-  console.log('Message sent', info.messageId)
   
-  res.redirect('success')
+  res.render('success', {title: 'Response', successMessage: true})
 })
 
 export default router
