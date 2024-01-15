3989d698f878fdfeabef8334b2e4b6e27e9ad615
Cristianunrc <cristiankhan23@gmail.com>
2024-01-15 16:33:05 -0300

    Configuration for send email

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
-
 import indexRoutes from './routes/index.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// configuration for send email
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import nodemailer from 'nodemailer'
 
 const router = Router()
 
@@ -7,5 +8,40 @@ router.get('/', (req, res) => res.render('index', { title: 'Home' }))
 router.get('/about', (req, res) => res.render('about', { title: 'About' }))
 
 router.get('/contact', (req, res) => res.render('contact', { title: 'Contact' }))
+
+router.get('/success', (req, res) => res.render('success'))
+
+router.post('/send-email', async (req, res) => {
+  const { email, message } = req.body;
+
+  const contentHTML =  `
+    <h1>Email information</h1>
+    <ul>
+      <li>Email: ${email}</li>
+    </ul>
+    <p>${message}</p>
+  `;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com', // forwardemail.net
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'cristiankhan23@gmail.com',
+      pass: 'xpdd jwja gjkg plnr'
+    }
+  })
+
+  const info = await transporter.sendMail({
+    from: 'cristiankhan23@gmail.com',
+    to: 'cristiankhan23@gmail.com',
+    subject: 'Website content form',
+    text: 'Hello world'
+  })
+
+  console.log('Message sent', info.messageId)
+  
+  res.redirect('success')
+})
 
 export default router
